647175257d2b61b15ff48011378f3e7fa9bf41f6
prateek145 <prateekk898@gmail.com>
2024-01-17 16:01:10 +0530

    local

--- resources/views/backend/email/after10_10.blade.php
+++ resources/views/backend/email/after10_10.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.email.layout.app')
 @section('content')
 <td class="wrapper">
-    <p> Hi "User" you have not create any task since morning, please create tasks for the day. </p>
+    <p> Hi {{$body['name'] ?? 'User'}} you have not create any task since morning, please create tasks for the day. </p>
     <h4><a href="{{route('login')}}" target="_blank">Project Management</a></h4>
 </td>
 @endsection
